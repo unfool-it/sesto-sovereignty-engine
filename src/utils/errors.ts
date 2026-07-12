c95c5e78962ec6b3bbbeb0617ab13e912d6f72ac
unfool.it <ermeticadesign@gmail.com>
2026-07-12 19:50:47 +0200

    Refactor error handling in errors.ts
    Refactor SovereigntyError to use code instead of statusCode. Introduce new error classes for SecurityError, AssetError, and ConfigError with specific codes.

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,12 +1,25 @@
 export class SovereigntyError extends Error {
-  constructor(public message: string, public statusCode: number = 400) {
-    super(message);
-    this.name = 'SovereigntyError';
-  }
+    constructor(public message: string, public code: string = 'INTERNAL_ERROR') {
+        super(message);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
 }
 
-export class AssetInsecureError extends SovereigntyError {
-  constructor(detail: string) {
-    super(`Security Degradation Detected: ${detail}`, 403);
-  }
+export class SecurityError extends SovereigntyError {
+    constructor(message: string) {
+        super(message, 'SECURITY_VIOLATION');
+    }
+}
+
+export class AssetError extends SovereigntyError {
+    constructor(message: string) {
+        super(message, 'ASSET_INVALID');
+    }
+}
+
+export class ConfigError extends SovereigntyError {
+    constructor(message: string) {
+        super(message, 'CONFIGURATION_INVALID');
+    }
 }
